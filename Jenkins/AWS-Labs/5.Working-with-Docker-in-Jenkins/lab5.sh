ec21528b5c3fb0afc636fad3769262f7e35ed5c7
AndyHuynh <andy.tranhuynh@gmail.com>
2025-02-25 12:56:56 +0200

    lab5

---
+++ Jenkins/AWS-Labs/5.Working-with-Docker-in-Jenkins/lab5.sh
@@ -0,0 +1,13 @@
+#Use the following repository:
+https://github.com/andylovecloud/nodejs-random-color
+#You can use it directly or fork it to your github
+
+#1. Create a pipeline job in Jenkins
+#2. In the pipeline job, use Jenkinsfile to perform the following steps:
+#- Checkout the source code from the repository on Git.
+#- Use Dockerfile to build the image
+#- Push the image to AWS ECR
+# View the lab5.groovy file
+#Trouble shoot error Permission denied
+sudo usermod -a -G docker jenkins
+sudo systemctl restart jenkins
