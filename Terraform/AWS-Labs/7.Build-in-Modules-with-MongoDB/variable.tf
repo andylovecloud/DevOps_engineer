a7e9b18179db711c732dcc7291efbd81ba0e16bd
AndyHuynh <andy.tranhuynh@gmail.com>
2025-03-06 11:09:35 +0200

    update code

--- Terraform/AWS-Labs/7.Build-in-Modules-with-MongoDB/variable.tf
+++ Terraform/AWS-Labs/7.Build-in-Modules-with-MongoDB/variable.tf
@@ -1,17 +1,38 @@
+variable "region" {
+  type = string
+}
+
+variable "workstation_ip" {
+  type = string
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "VPC cidr block. Example: 10.10.0.0/16"
+}
+
 variable "availability_zones" {
   type = list(any)
 }
-variable "vpc_name" {
-  type = string  
-}
-variable "cidr_block" {
+
+variable "keypair_path" {
   type = string
 }
-variable "public_subnet_ips" {
-  type = list(string)
-  description = "List of public subnet IPs CIDR"
+variable "bastion_instance_type" {
+  type = string
 }
-variable "private_subnet_ips" {
-  type = list(string)
-  description = "List of private subnet IPs CIDR"
+variable "bastion_ami" {
+  type = string
+}
+variable "app_instance_type" {
+  type = string
+}
+variable "app_ami" {
+  type = string
+}
+variable "db_instance_type" {
+  type = string
+}
+variable "db_ami" {
+  type = string
 }
